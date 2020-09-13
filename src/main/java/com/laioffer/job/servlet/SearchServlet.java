70de3c3683919ab8ecb44c2aa592954a6455391d
ximin <h4nn11113@gmail.com>
2020-09-13 15:16:36 -0700

    create the SearchServlet, check if GitHubClient works

--- src/main/java/com/laioffer/job/servlet/SearchServlet.java
+++ src/main/java/com/laioffer/job/servlet/SearchServlet.java
@@ -1,5 +1,7 @@
 package com.laioffer.job.servlet;
 
+import com.laioffer.job.external.GitHubClient;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -7,13 +9,22 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "TestServlet", urlPatterns = {"/test"})
-public class TestServlet extends HttpServlet {
+@WebServlet(name = "SearchServlet", urlPatterns = {"/search"})
+
+public class SearchServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.getWriter().print("Hello World");
+        double lat = Double.parseDouble(request.getParameter("lat"));
+        double lon = Double.parseDouble(request.getParameter("lon"));
+
+        GitHubClient client = new GitHubClient();
+        String itemsString = client.search(lat, lon, null);
+
+        response.setContentType("application/json");
+        response.getWriter().print(itemsString);
+
     }
 }
